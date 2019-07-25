f51cb81f8e998a2e4512eabcba49cfcaf79c662e
Andrew Carlisle <ohmygodadinosaur@gmail.com>
2019-07-25 23:57:18 +0000

    Added some validation to the syaing and author fields in the quote submission form. Also added a custom message and return to index when bad data is enterred, but I haven't actually seen this functionality since the form does not allow submission with invlaid data.  It automatically displays an error underneath the field.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ApplicationRecord
+  # Ensures the enterred quote will not be blank; will be between 3 and 500
+  #  characters long
+  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 2 }
 end
