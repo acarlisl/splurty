249f268eb2267324d2f23030bbbccc80de1957e7
Andrew Carlisle <ohmygodadinosaur@gmail.com>
2019-07-26 03:08:12 +0000

    Built the 'about' page, including styling adding functionality to the link to get there.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -17,6 +17,11 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+  def about 
+
+  end
+
+
   private
 
   def quote_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'quotes#index'
   resources :quotes
+
+  # Add a route to our About page
+  get 'about', to: 'quotes#about'
 end
