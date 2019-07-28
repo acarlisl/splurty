074e73f8d81641010625a133f67456ee32ffce7e
Andrew Carlisle <ohmygodadinosaur@gmail.com>
2019-07-28 19:00:34 +0000

    Fixed the error handling when enterring a new quote. Was forgetting to grab the newly created quote before attempting to run invalid? on it. Also needed to rake the migration file again and restart the server. MAKE SURE YOU DO THIS WHEN MOVED TO PRODUCTION!

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,5 +2,5 @@ class Quote < ApplicationRecord
   # Ensures the enterred quote will not be blank; will be between 3 and 500
   #  characters long
   validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
-  validates :author, presence: true, length: { maximum: 50, minimum: 2 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
